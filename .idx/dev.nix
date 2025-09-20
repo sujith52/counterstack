56f7e51ed7247e5d7228adf794c093425a81d4c6
sujithhemanth52 <sujithhemanth52@gmail.com>
2025-09-20 02:33:24 +0000

    Step 1: Initial semantic search prototype with FastAPI + FAISS

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,9 +6,9 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
   # Sets environment variables in the workspace
